1a0eaf7face6b114a46584099e2f8e2880d52220
victor attar <vsla@cin.ufpe.br>
2024-04-30 21:51:20 -0300

    Feat - Added date to article

--- src/app/components/ArtciclesList/components/ArticleComponent/index.tsx
+++ src/app/components/ArtciclesList/components/ArticleComponent/index.tsx
@@ -6,7 +6,10 @@ import { Article } from '@/app/types'
 import Image from 'next/image'
 import Typography from '@mui/material/Typography';
 
-export const ArticleComponent = ({ article: { title, urlToImage, description, } }: { article: Article }) => {
+export const ArticleComponent = ({ article: { title, urlToImage, description, publishedAt } }: { article: Article }) => {
+
+  const articleDate = new Date(publishedAt)
+
   return (
     <ArticleContainer>
       <ImageComponent src={urlToImage} alt={title}
@@ -19,16 +22,24 @@ export const ArticleComponent = ({ article: { title, urlToImage, description, } 
       </Typography>
 
       <Typography variant="subtitle2" gutterBottom>
-        {description} <span></span>
+        {description}
       </Typography>
+      <DateContainer variant='body2'>{articleDate.toLocaleString()}</DateContainer>
     </ArticleContainer>
   )
 }
 
 const ArticleContainer = styled.div`
   margin: 12px;
+  
+  img{
+    &:hover{
+      filter: blur(0.05rem);
+    }
+  }
+
   &:hover{
-   color: #595959 ;
+   color: #8a2e1b ;
    cursor: pointer;
   }
 `
@@ -37,4 +48,9 @@ const ImageComponent = styled(Image)`
   object-fit: contain;
   width: 100%;
   height: auto;
+  border-radius: 8px;
+`
+
+const DateContainer = styled(Typography)`
+  color: #7c7c7c
 `
