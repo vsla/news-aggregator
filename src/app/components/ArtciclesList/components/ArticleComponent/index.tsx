1dcc28a665cdedceb2c475363f0c1910118900a7
victor attar <vsla@cin.ufpe.br>
2024-05-02 11:47:55 -0300

    Added new york times source

--- src/app/components/ArtciclesList/components/ArticleComponent/index.tsx
+++ src/app/components/ArtciclesList/components/ArticleComponent/index.tsx
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 export const ArticleComponent = ({ article: { title, urlToImage, description, publishedAt } }: { article: Article }) => {
 
   const articleDate = new Date(publishedAt)
-  console.log(urlToImage)
+
   return (
     <ArticleContainer>
       <ImageComponent src={urlToImage} alt={title}
--- src/app/contexts/Articles/actions/fetchArticles.ts
+++ src/app/contexts/Articles/actions/fetchArticles.ts
@@ -22,12 +22,55 @@ type theGuardianArticleAsset = {
 type theGuardianArticle = {
   webPublicationDate: string
   webTitle: string
-  webUrl: string,
   type: string,
   elements: Array<
     {
       assets: theGuardianArticleAsset[]
     }>
+}
+
+type multimediaNYT = {
+  format: string,
+  width: string,
+  url: string,
+  type: string
+}
+
+type NYTArticle = {
+  title: string
+  abstract: string,
+  item_type: string,
+  created_date: string
+  multimedia: multimediaNYT[]
+}
+
+const formatNYT = (articles: NYTArticle[]) => {
+  const formattedArticles: Article[] = []
+
+  const findTheLargerImage = (assets: multimediaNYT[]) => assets.find(({ format }) => {
+    return format === 'mediumThreeByTwo440'
+  })
+
+  articles.forEach((
+    { abstract,
+      title,
+      multimedia, item_type, created_date }
+  ) => {
+
+    if (multimedia && item_type === 'Article') {
+
+      const imageUrl = findTheLargerImage(multimedia)?.url || ''
+
+      formattedArticles.push({
+        title,
+        description: abstract,
+        urlToImage: imageUrl,
+        publishedAt: created_date
+      })
+    }
+  })
+
+  return formattedArticles
 }
 
 const formatTheGuardian = (articles: theGuardianArticle[]) => {
@@ -69,22 +112,24 @@ const removeArticleWithoutImagesFromNewsApi = (articles: newsApiArticle[]) => {
 }
 
 export async function fetchArticles() {
-  const { NEWS_API_API_KEY, THE_GUARDIAN_API_KEY } = process.env
+  const { NEWS_API_API_KEY, THE_GUARDIAN_API_KEY, NYT_API_KEY } = process.env
 
   const response1: Promise<{ articles: newsApiArticle[] }> = fetch(`https://newsapi.org/v2/top-headlines?apiKey=${NEWS_API_API_KEY}&sources=bbc-news`, { next: { revalidate: 0 } }).then((response) => response.json())
   const response2: Promise<{ response: { results: theGuardianArticle[] } }> = fetch(`https://content.guardianapis.com/search?show-elements=image&api-key=${THE_GUARDIAN_API_KEY}`, { next: { revalidate: 0 } }).then((response) => response.json())
+  const response3: Promise<{ results: NYTArticle[] }> = fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${NYT_API_KEY}`, { next: { revalidate: 0 } }).then((response) => response.json())
 
   let articles: Article[] = []
-  await Promise.all([response1, response2]).then((values) => {
+  await Promise.all([response1, response2, response3]).then((values) => {
 
-    const formattedGuardian = formatTheGuardian(values[1].response.results)
     const formattedNewsApi = removeArticleWithoutImagesFromNewsApi(values[0].articles)
+    const formattedGuardian = formatTheGuardian(values[1].response.results)
+    const formattedNYT = formatNYT(values[2].results)
 
-    articles = [...formattedGuardian, ...formattedNewsApi,]
+    articles = [...formattedGuardian, ...formattedNewsApi, ...formattedNYT]
   });
 
   return {
-    total: 0,
+    total: articles.length,
     articles: articles
   }
 }
